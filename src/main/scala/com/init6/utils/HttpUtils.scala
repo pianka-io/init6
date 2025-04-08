e00f5d771935f7d074a512613af062fc40f3ff29
Rick Pianka <rick@pianka.io>
2025-04-08 08:54:24 -0400

    fix(http): turning off d2-activity for now, guessing this sync call causes lag

--- src/main/scala/com/init6/utils/HttpUtils.scala
+++ src/main/scala/com/init6/utils/HttpUtils.scala
@@ -5,16 +5,16 @@ import okhttp3._
 object HttpUtils {
   private val client = new OkHttpClient()
 
-  def postMessage(url: String, message: String): String = {
-    val requestBody = RequestBody.create(MediaType.get("application/json"), s"""{"message": "$message"}""")
-
-    val request = new Request.Builder()
-      .url(url)
-      .post(requestBody)
-      .build()
-
-    val response = client.newCall(request).execute()
-
-    response.body().string()
+  def postMessage(url: String, message: String) = {
+//    val requestBody = RequestBody.create(MediaType.get("application/json"), s"""{"message": "$message"}""")
+//
+//    val request = new Request.Builder()
+//      .url(url)
+//      .post(requestBody)
+//      .build()
+//
+//    val response = client.newCall(request).execute()
+//
+//    response.body().string()
   }
 }
