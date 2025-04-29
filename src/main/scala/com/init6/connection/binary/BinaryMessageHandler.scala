5613b7a03f6918983a6f729d24cdd1c5b420d015
Christopher Baris <chrisbaris87@gmail.com>
2025-04-28 22:51:08 -0400

    Revert "Disabled keepalive/ping checks to prevent disconnects"
    This reverts commit 5720d8e08a87bda33ab7a512bcf695eeab9aa497.

--- src/main/scala/com/init6/connection/binary/BinaryMessageHandler.scala
+++ src/main/scala/com/init6/connection/binary/BinaryMessageHandler.scala
@@ -173,7 +173,7 @@ class BinaryMessageHandler(connectionInfo: ConnectionInfo) extends Init6KeepAliv
           case SidEnterChat(packet) =>
             send(SidEnterChat(username, oldUsername, productId))
             send(BinaryChatEncoder(UserInfoArray(Config().motd)).get)
-            //keepAlive(actor, () => sendPing())
+            keepAlive(actor, () => sendPing())
             goto(LoggedIn)
         }
 //      case SID_LEAVECHAT =>
@@ -538,7 +538,7 @@ class BinaryMessageHandler(connectionInfo: ConnectionInfo) extends Init6KeepAliv
             case SidEnterChat(packet) =>
               send(SidEnterChat(username, oldUsername, productId))
               send(BinaryChatEncoder(UserInfoArray(Config().motd)).get)
-              //keepAlive(actor, () => sendPing())
+              keepAlive(actor, () => sendPing())
               goto(LoggedIn)
           }
         case _ => handleRest(BinaryPacket(packetId, data))
